4b200594d5f75250f6620deeb0b2e70bb6408582
Md Tauqueer Raza <mdtraza96@gmail.com>
2024-10-26 20:57:34 +0530

    Initial commit of contact form

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    console.log('Name:', name);
+    console.log('Email:', email);
+    console.log('Message:', message);
+
+    document.getElementById('successMessage').classList.remove('hidden');
+    document.getElementById('contactForm').reset(); 
+});
